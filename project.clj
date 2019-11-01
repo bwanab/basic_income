4bf29f2a1c652c8fb5f26f06d54f822887c1b222
Bill Allen <bill@k2bea.org>
2019-10-31 21:07:36 -0400

    update to recent clojure and incanter

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incanter "1.5.2"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [incanter/incanter-core "1.9.3"]
+                 [incanter/incanter-charts "1.9.3"]])
